ae1c88d4fe2e55db0aa76da9fed1473a3954f5a1
Scott <scott.heron@outlook.com>
2016-03-02 11:20:21 -0800

    added counter that I forgot in guessing game and added alternative code from Josh

--- bank_transactions.rb
+++ bank_transactions.rb
@@ -37,3 +37,53 @@ begin
     puts "Another transaction? yes or no"
     answer = gets.chomp
 end while answer == "yes"
+
+# #Alternative Code
+# BANK_DEPOSIT = 'deposit'
+# BANK_WITHDRAW = 'withdraw'
+# BANK_CHECK_BALANCE = 'check balance'
+
+# class Bank
+#     attr_reader :balance
+    
+#     def initialize
+#         @balance = 0
+#     end
+
+#     def deposit(amt)
+#         @balance += amt
+#     end
+
+#     def withdraw(amt)
+#         if amt <= @balance
+#             @balance -+ amt
+#         end
+#     end
+# end
+
+# done = false
+
+# bank = Bank.new
+
+# puts 'Your current balance is: '+bank.balance.to_s
+# while done == false
+#     puts 'What do you want to do? (deposit, withdraw, check balance)'
+#     op = gets.chomp
+#     if op == BANK_DEPOSIT
+#         puts 'How much?'
+#         deposit = gets.chomp.to_f
+#         bank.deposit(deposit)
+#     elsif op == BANK_WITHDRAW
+#         puts 'How much?'
+#         withdraw = gets.chomp.to_f
+#         bank.withdraw(withdrawal)
+#     elsif op == BANK_CHECK_BALANCE
+#         puts 'Your balance is: '+bank.balance.to_s
+#     else
+#         puts 'Huh?'
+#     end
+#     puts 'Are you done?'
+#     response = gets.chomp
+#         done = response == 'yes'
+        
+# end
--- guessing_game.rb
+++ guessing_game.rb
@@ -1,16 +1,18 @@
 class Guess
     
     def guess_higher_or_lower
-        @@computer_guess = rand(1..10)
+        @@computer_guess = rand(1..100)
+        tries = 0
         begin
-            puts "Guess a number between 1 and 10:"
+            tries += 1
+            puts "Guess a number between 1 and 100:"
             @@user_guess = gets.chomp
             if @@user_guess.to_i > @@computer_guess
                 puts "Too high, try again"
             elsif @@user_guess.to_i < @@computer_guess
                 puts "Too low, try again"
             elsif @@user_guess.to_i == @@computer_guess
-                puts "CORRECT!"
+                puts "CORRECT! Found it in #{tries} tries"
             end
         end while @@user_guess.to_i != @@computer_guess
         
@@ -20,3 +22,36 @@ end
 guess = Guess.new
 
 guess.guess_higher_or_lower
+
+# #Alternate code
+
+# class Guess
+#     @number = 0
+
+#     def initialize
+#         @number = rand(1..100)
+#     end
+
+#     def play
+#         puts 'guess a number between 1 and 100'
+#         correct = false
+#         tries = 0
+
+#         while correct == false  
+#             tries += 1
+#             guess = gets.chomp.to_i
+#             if guess > @number
+#                 puts "Too high"
+#             elsif guess < @number
+#                 puts 'too low'
+#             else
+#                 puts "Correct, found it in #{tries} tries"
+#                 correct = true
+#             end
+
+#     end
+# end
+
+# game = Guess.new
+
+# guess.play
